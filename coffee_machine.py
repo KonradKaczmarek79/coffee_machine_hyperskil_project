3d7f20ea2a574de2433af20fa6a17cc8997c914d
Konrad Kaczmarek <55359118+KonradKaczmarek79@users.noreply.github.com>
2025-04-19 16:41:01 +0200

    Update coffee_machine.py add methods associated with coffe bying

--- coffee_machine.py
+++ coffee_machine.py
@@ -26,3 +26,34 @@ class CoffeeMachine:
                 f"{self.coffee_beans} g of coffee beans\n"
                 f"{self.disposable_cups} disposable cups\n"
                 f"${self.money} of money")
+        
+    def __buy_espresso(self):
+        """Changes the values for each supply according to the ones needed to prepare espresso."""
+        self.money += 4
+        self.water -= 250
+        self.coffee_beans -= 16
+        self.disposable_cups -= 1
+
+    def __buy_latte(self):
+        """Changes the values for each supply according to the ones needed to prepare latte."""
+        self.money += 7
+        self.water -= 350
+        self.milk -= 75
+        self.coffee_beans -= 20
+        self.disposable_cups -= 1
+
+    def __buy_cappuccino(self):
+        """Changes the values for each supply according to the ones needed to prepare cappuccino."""
+        self.money += 6
+        self.water -= 200
+        self.milk -= 100
+        self.coffee_beans -= 12
+        self.disposable_cups -= 1
+
+    def buy_cup_of_coffee(self, kind_of_coffee):
+        if kind_of_coffee == "1":
+            self.__buy_espresso()
+        elif kind_of_coffee == "2":
+            self.__buy_latte()
+        elif kind_of_coffee == "3":
+            self.__buy_cappuccino()
